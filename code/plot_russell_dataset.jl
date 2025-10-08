cc55ceece5d6801dbf5577a0cd1d9c2386814cd0
Harshit Shah <harshit@Harshits-MacBook-Air.local>
2025-10-08 12:54:38 +0530

    plot data in a better/cleaner way

--- code/plot_russell_dataset.jl
+++ code/plot_russell_dataset.jl
@@ -3,33 +3,40 @@ import CSV
 import DataFrames
 import Plots
 
+# Set the plot theme to dark for a professional look
+# Plots.theme(:dark)
+
 # Define the path to the dataset
 file_path = "/Users/harshit/Downloads/Research-Commons-Quant/SciML-Julia/russell-datasets/growth_value_cumulative_returns_corrected.csv"
 
 # Load the dataset into a DataFrame
 df = CSV.read(file_path, DataFrames.DataFrame)
 
-# Create the time-series plot
-# The row number is used as the implicit x-axis (time)
+# Create the time-series plot using the Date column for the x-axis
 p = Plots.plot(
+    df.Date,
     df.growth_cumulative_ret,
     label="Growth Cumulative Returns",
-    xlabel="Time Step",
+    xlabel="Date",
     ylabel="Cumulative Returns",
-    title="Russell Growth vs. Value Cumulative Returns",
-    legend=:outertopright
+    title="Cumulative Returns of Growth and Value ETFs",
+    legend=:topleft
 )
 
 Plots.plot!(
     p,
+    df.Date,
     df.value_cumulative_ret,
     label="Value Cumulative Returns"
 )
 
-# Save the plot to a file
-Plots.savefig(p, "cumulative_returns_dataset_plot.png")
+# Add a grid
+Plots.plot!(grid=true)
 
-println("Plot saved to cumulative_returns_dataset_plot.png")
+# Save the plot to a file
+Plots.savefig(p, "cumulative_returns_dataset_plot_styled.png")
+
+println("Styled plot saved to cumulative_returns_dataset_plot_styled.png")
 
 # To run this file, open the Julia REPL in your project directory and execute:
 # include("code/plot_russell_dataset.jl")
