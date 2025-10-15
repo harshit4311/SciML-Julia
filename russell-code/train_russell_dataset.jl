739dea94f48fffbc730591350c956838d4cb4efe
Harshit Shah <harshit@Harshits-MacBook-Air.local>
2025-10-15 14:06:58 +0530

    added curves in the synthetic dataset

--- russell-code/train_russell_dataset.jl
+++ russell-code/train_russell_dataset.jl
@@ -27,7 +27,7 @@ using Statistics: mean, std, quantile
 # ---------------------------------------
 # Load SYNTHETIC Russell dataset
 # ---------------------------------------
-df = CSV.read("/Users/harshit/Downloads/Research-Commons-Quant/SciML-Julia/russell-datasets/synthetic-russell-dataset-200.csv", DataFrames.DataFrame)
+df = CSV.read("/Users/harshit/Downloads/Research-Commons-Quant/SciML-Julia/russell-datasets/LV_synthetic_russell_growth_value_200.csv", DataFrames.DataFrame)
 
 # Extract columns
 growth = df.Growth
